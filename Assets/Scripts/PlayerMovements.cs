ee17d8b6ba9be4e73632ad7c8bb1ed5c9dd5e81a
RiviWickramarachchi <wickramarachchi.rivindu@gmail.com>
2023-05-08 21:53:33 +0200

    logic for the climbing mechanics were added to the player movement script

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -11,14 +11,24 @@ public class PlayerMovements : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     private Rigidbody2D rb;
     private float horizontal;
+    private float gravityScale = 1f;
+    private float vertical;
+    private bool ladderTriggered = false;
     private bool isFacingRight = true;
+    private bool isClimbing = false;
     void Awake()
     {
         rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() {
+
+        if(ladderTriggered && Mathf.Abs(vertical) > 0) {
+            isClimbing = true;
+        }
+    }
+    void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
@@ -28,10 +38,19 @@ public class PlayerMovements : MonoBehaviour
         else if (isFacingRight && horizontal < 0f) {
             FlipCharacter();
         }
+
+        if(isClimbing) {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, vertical * speed);
+        }
+        else {
+            rb.gravityScale = gravityScale;
+        }
     }
 
     public void PlayerMove(InputAction.CallbackContext context) {
         horizontal = context.ReadValue<Vector2>().x;
+        vertical = context.ReadValue<Vector2>().y;
     }
 
     public void PlayerJump (InputAction.CallbackContext context) {
@@ -50,5 +69,18 @@ public class PlayerMovements : MonoBehaviour
         Vector3 localscale = transform.localScale;
         localscale.x = localscale.x * -1f;
         transform.localScale = localscale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.CompareTag("Ladder")) {
+            ladderTriggered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if(collision.CompareTag("Ladder")) {
+            ladderTriggered = false;
+            isClimbing = false;
+        }
     }
 }
